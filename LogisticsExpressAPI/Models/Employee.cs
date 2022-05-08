faeeb62b666a2da7608a63e1964360ea78501910
ClairisaMoodley <u20440112@tuks.co.za>
2022-05-08 16:16:11 +0200

    EmployeeAPI added

---
+++ LogisticsExpressAPI/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Emp_PhoneNumber { get; set; } = String.Empty;
+        public string Emp_Email { get; set; } = String.Empty;
+        public string Emp_Contact { get; set; } = String.Empty;
+        public string Emp_FirstName { get; set; } = String.Empty;
+        public string Emp_LastName { get; set; } = String.Empty;
+
+        [JsonIgnore]
+        public EmployeeRole? EmployeeRole { get; set; } //MANY employees ONE employee role
+        public int EmployeeRoleId { get; set; }
+    }
+}

737a8932d2e68548728582cdb68193df4c33465d
ClairisaMoodley <u20440112@tuks.co.za>
2022-05-22 23:37:07 +0200

    Edit route.

--- LogisticsExpressAPI/Controllers/RepairRequests.cs
+++ LogisticsExpressAPI/Controllers/RepairRequests.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using RequestAndFuelAPI.Data;
+using LogisticsExpressAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using LogisticsExpressAPI.Models;
 

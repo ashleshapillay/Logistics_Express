e9a546a894e2232f7b8d21b1a5f07cfdb5f25680
ClairisaMoodley <u20440112@tuks.co.za>
2022-05-25 12:46:04 +0200

    Updated controller to avoid image issues.

--- LogisticsExpressAPI/Controllers/FuelController.cs
+++ LogisticsExpressAPI/Controllers/FuelController.cs
@@ -40,39 +40,34 @@ namespace LogisticsExpressAPI.Controllers
         }
         
 
+
         //Add a fuel entry
         [HttpPost]
-        public async Task<IActionResult> AddFuelEntry(int vehicleId,decimal litres,decimal priceLitre,decimal totalSpent, IFormFile file)
+        public async Task<IActionResult> AddFuelEntry(FuelLog fuelLog)
         {
-            FuelLog fuelLog = new FuelLog();
-            fuelLog.Log_Date = DateTime.Now;
-            fuelLog.Litres = litres;
-            fuelLog.Price_Per_Litre = priceLitre;
-            fuelLog.Total_Spent = totalSpent;
-            fuelLog.Receipt_Image = file.FileName;
-            fuelLog.VehicleId = vehicleId;
-
             dataContext.FuelLogs.Add(fuelLog);
             await dataContext.SaveChangesAsync();
 
             return CreatedAtAction("GetFuelEntry", new { id = fuelLog.FuelEntryId }, fuelLog);
 
+            //IMAGE
         }
+
 
         //Update a fuel entry
         [HttpPut]
         [Route("{id:int}")]
-        public async Task<IActionResult> UpdateFuelEntry([FromRoute] int id, decimal litres, decimal priceLitre, decimal totalSpent, int vehicleId, IFormFile file)
+        public async Task<IActionResult> UpdateFuelEntryt([FromRoute] int id, [FromBody] FuelLog fuelLog)
         {
             var existingFuelEntry = await dataContext.FuelLogs.FirstOrDefaultAsync(x => x.FuelEntryId == id);
             if (existingFuelEntry != null)
             {
-                existingFuelEntry.Log_Date = DateTime.Now;
-                existingFuelEntry.Litres = litres;
-                existingFuelEntry.Price_Per_Litre = priceLitre;
-                existingFuelEntry.Total_Spent = totalSpent;
-                existingFuelEntry.Receipt_Image = file.FileName;
-                existingFuelEntry.VehicleId = vehicleId;
+                existingFuelEntry.Log_Date = fuelLog.Log_Date;
+                existingFuelEntry.Litres = fuelLog.Litres;
+                existingFuelEntry.Price_Per_Litre = fuelLog.Price_Per_Litre;
+                existingFuelEntry.Total_Spent = fuelLog.Total_Spent;
+                existingFuelEntry.ReceiptImage = fuelLog.ReceiptImage;
+
                 await dataContext.SaveChangesAsync();
                 return Ok(existingFuelEntry);
             }

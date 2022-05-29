46f6c9307168645810368458269a3ce8d3c8bd5c
meganchetty <84287727+meganchetty@users.noreply.github.com>
2022-05-29 19:38:24 +0200

    Vehicle Controller & Model

---
+++ LogisticsExpressAPI/Models/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle_Part2_API.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleID { get; set; }
+        public string TareWeight { get; set; }
+
+        [JsonIgnore]
+        public VehicleModel? VehicleModel { get; set; }
+        public int VehicleModelID { get; set; }
+        public VehicleMake? VehicleMake { get; set; }
+        public int VehicleMakeID { get; set; }
+        public VehicleType? VehicleType { get; set; }
+        public int VehicleTypeID { get; set; }
+
+        //public InsurancePlan? InsurancePlan { get; set; }
+        //public int InsurancePlanID { get; set; }
+
+        //public VehicleLisence? VehicleLisence { get; set; }
+        //public int VehicleLisenceID { get; set; }
+
+        //public ServicePlan? ServicePlan {get;set;}
+        //public int ServicePlanID { get; set; }
+
+
+    }
+}

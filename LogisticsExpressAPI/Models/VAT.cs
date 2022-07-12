f891c694289a25084ef4fa5e3a40303a9654aea4
ClairisaMoodley <u20440112@tuks.co.za>
2022-07-12 22:26:15 +0200

    VAT backend - Create and edit

---
+++ LogisticsExpressAPI/Models/VAT.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace LogisticsExpressAPI.Models
+{
+    public class VAT
+    {
+        [Key]
+        public int VATId { get; set; }
+        public double VATPercentage { get; set; }
+    }
+}
